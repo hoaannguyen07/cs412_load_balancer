f64bff39f944188c985e668eb3f7cd4e58905377
Hoa Nguyen <hoaannguyen07@gmail.com>
2022-10-02 22:08:25 -0500

    feature: create entrypoint to load balance and handle user inputs
    There exists default mode & custom mode. Inputs are mode type, num cycles, and num servers.

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+enum Option {DEFAULT, CUSTOM, INVALID};
+
+void output_parameters_of_load_balancer(int& num_cycles, int& num_servers)
+{
+    std::cout << "Starting off with " << num_cycles << " clock cycles and " << num_servers << " servers\n";
+}
+
+Option handle_input(int &num_cycles, int &num_servers)
+{
+    char option_char;
+    Option option = INVALID;
+    while(option == INVALID){    
+        std::cout << "How would you like to run the Load Balancer? [d]efault or [c]ustom? ";
+        std::cin >> option_char; 
+
+        switch (option_char)
+        {
+            case 'd':
+                option = DEFAULT;
+                break;
+            case 'c':
+                option = CUSTOM;
+                std::cout << "How many cycles do you want to run the Load Balancer for? ";
+                std::cin >> num_cycles;
+                std::cout << "How many servers do you want to start off with? ";
+                std::cin >> num_servers;
+                break;
+            default:
+                std::cout << "Invalid option. Please choose a valid options\n";
+                break;
+        }
+    }
+    output_parameters_of_load_balancer(num_cycles, num_servers);
+    return option;
+}
+
+int main()
+{
+    int num_cycles = 10000, num_servers = 10;
+    Option chosen_option = handle_input(num_cycles, num_servers);
+    return 0;
+}
