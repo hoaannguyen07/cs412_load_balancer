dc1c52a544dab06a545a76a259a95d51281a527c
Hoa Nguyen <hoaannguyen07@gmail.com>
2022-10-07 11:05:54 -0500

    chore: Add documentation to the Server header file

--- server.hpp
+++ server.hpp
@@ -5,6 +5,10 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+/**
+ * @brief A class used to handle requests
+ * 
+ */
 class Server
 {
 private:
@@ -15,14 +19,50 @@ private:
 
     bool isReadyToResolveRequest(int);
 public:
-    Server(std::string);
+    /**
+     * @brief Construct a new Server object with a passed in name
+     * 
+     * @param serverName name of the created Server instance
+     */
+    Server(std::string serverName);
     ~Server();
 
+    /**
+     * @brief Check if the server is available to process a new request
+     * 
+     * @return true Server is available to process a new request
+     * @return false Server is NOT available to process a new request
+     */
     bool isAvailable();
-    void assignRequest(Request*, int);
-    void attemptResolvingRequest(int);
+
+    /**
+     * @brief Assign a request to the server for handling
+     * 
+     * @param request The request the server is supposed to handle
+     * @param curTime The current clock time
+     */
+    void assignRequest(Request* request, int curTime);
+
+    /**
+     * @brief Have the server attempt resolving the request.
+     * If the current time indicates that the request still has not finished processing, nothing will happen. 
+     * If the current time indicates that the request can be finished, a summary of the server handling the request will be printed out and the server will prepare to handle the next request
+     * 
+     * @param curTime 
+     */
+    void attemptResolvingRequest(int curTime);
+
+    /**
+     * @brief Reset the server state to prepare to handle a new request. Ensure that the server will be available if it is not already.
+     * 
+     */
     void resetServer();
 
+    /**
+     * @brief Summarize the state of the server instance
+     * 
+     * @return std::string 
+     */
     std::string to_string();
 };
 
