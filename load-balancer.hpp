046ef826d13e93f2e17405d01118bffa8bdacdb1
Hoa Nguyen <hoaannguyen07@gmail.com>
2022-10-09 01:46:28 -0500

    update(LoadBalancer): Update documentation of LoadBalancer
    Add "@details" tag to class description and "processNewRequestAndUpdateServerStates()" method to not show so much details on hover
    
    Used "@retval" to show multiple return values for "processNewRequestAndUpdateServerStates()" method

--- load-balancer.hpp
+++ load-balancer.hpp
@@ -9,7 +9,7 @@
 /**
  * @brief Class used to assign requests to individual servers in server groups to distribute workloads appropriately
  * 
- * This class uses the Round-Robin method to assign requests to servers for processing. This means that the next available server handles the incoming request. Due to the simplicity of this load balancer, server groups consisting of servers with the same specs will be best for this load balancer because the speed at which a request can be processed will not vary. Assigning a request to a specific server will not slow down the server more than if it was assigned to another server.
+ * @details This class uses the Round-Robin method to assign requests to servers for processing. This means that the next available server handles the incoming request. Due to the simplicity of this load balancer, server groups consisting of servers with the same specs will be best for this load balancer because the speed at which a request can be processed will not vary. Assigning a request to a specific server will not slow down the server more than if it was assigned to another server.
  * 
  */
 class LoadBalancer
@@ -35,12 +35,12 @@ public:
     /**
      * @brief Attempt to process a request and update the time for all servers to finish up processing their requests if possible
      * 
-     * Update the current time for servers in the server group in order to resolve any requests a server has that can be reolved. Then, find an available server to handle the request being passed in
+     * @details Update the current time for servers in the server group in order to resolve any requests a server has that can be reolved. Then, find an available server to handle the request being passed in
      * 
-     * @param request 
-     * @param curTime 
-     * @return true Request has been picked up by a server and is processing
-     * @return false Request has not been picked up yet OR request == nullptr (no request to process)
+     * @param (Request*) request 
+     * @param int curTime 
+     * @retval TRUE if request has been picked up by a server and is processing
+     * @retval FALSE if request has not been picked up yet OR request == nullptr (no request to process)
      */
     bool processNewRequestAndUpdateServerStates(Request* request, int curTime);
 
